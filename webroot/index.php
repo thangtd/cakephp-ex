7a7adf19d02bac3c6620ceb0ad84e891ec6c4d09
Henry Tran <thangtd@dtn.com.vn>
2018-05-11 22:15:25 +1000

    Clean up test code

--- webroot/index.php
+++ webroot/index.php
@@ -32,7 +32,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 use App\Application;
 use Cake\Http\Server;
-echo "Test Commit";
+
 // Bind your application to the server.
 $server = new Server(new Application(dirname(__DIR__) . '/config'));
 
